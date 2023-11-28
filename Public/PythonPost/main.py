7b314993decf5355be5da2817800274c3e362f46
rezlen <soheilyreza@yahoo.com>
2023-11-28 17:22:33 +0000

    DB Cockroaches added

--- Public/PythonPost/main.py
+++ Public/PythonPost/main.py
@@ -3,7 +3,14 @@ from pyscript import document
 
 
 def translate_english(event):
-    input_text = document.querySelector("#english")
-    english = input_text.value
-    output_div = document.querySelector("#output")
-    output_div.innerText = arrr.translate(english)
+  input_text = document.querySelector("#typeEnglish")
+  english = input_text.value
+  output_div = document.querySelector("#output")
+  output_div.innerText = arrr.translate(english)
+
+#  taking input & storing posts
+def displayPosts(event):
+  inputPost = document.querySelector("#CommentField")
+  posts = inputPost.value
+  outputDiv = document.querySelector("#PostOutput")
+  outputDiv.innerText = arrr.translate(posts)
