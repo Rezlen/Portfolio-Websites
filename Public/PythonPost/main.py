f8c36cb9f5d1822adef555a48dab071b3e248b09
rezlen <soheilyreza@yahoo.com>
2023-11-29 21:46:35 +0000

    .env added

--- Public/PythonPost/main.py
+++ Public/PythonPost/main.py
@@ -14,3 +14,52 @@ def displayPosts(event):
   posts = inputPost.value
   outputDiv = document.querySelector("#PostOutput")
   outputDiv.innerText = arrr.translate(posts)
+
+
+'''
+# connecting to databse https://app.planetscale.com/soheilyreza/portfoliowebsite/connect
+# Load environment variables from the .env file
+from dotenv import load_dotenv
+load_dotenv()
+import os
+import MySQLdb
+
+# Connect to the database
+connection = MySQLdb.connect(
+  host=os.getenv("aws.connect.psdb.cloud"),
+  user=os.getenv("kqppw0knmqo57dq66i0h"),
+  passwd=os.getenv("pscale_pw_9AVjqamDVhw4IhO2qrpkyLMU7NKHANJLu4Dtdyh38A"),
+  db=os.getenv("portfoliowebsite"),
+  autocommit=True,
+  ssl_mode="VERIFY_IDENTITY",
+  # See https://planetscale.com/docs/concepts/secure-connections#ca-root-configuration
+  # to determine the path to your operating systems certificate file.
+  ssl={ "ca": "" }
+)
+
+try:
+    # Create a cursor to interact with the database
+    cursor = connection.cursor()
+
+    # Execute "SHOW TABLES" query
+    cursor.execute("SHOW TABLES")
+
+    # Fetch all the rows
+    tables = cursor.fetchall()
+
+    # Print out the tables
+    print("Tables in the database:")
+    for table in tables:
+        print(table[0])
+
+except MySQLdb.Error as e:
+    print("MySQL Error:", e)
+
+finally:
+    # Close the cursor and connection
+    cursor.close()
+    connection.close()
+    
+    
+'''
+
